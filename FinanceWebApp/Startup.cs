6c0bd61f634c6d899bb8a32435f6f6061dde8ac6
keelerjr12 <keelerjr12@gmail.com>
2020-04-25 07:08:29 -0500

    Added forwarded proxies

--- FinanceWebApp/Startup.cs
+++ FinanceWebApp/Startup.cs
@@ -1,6 +1,7 @@
 using FinanceDataAccess;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -42,6 +43,11 @@ namespace FinanceWebApp
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseForwardedHeaders(new ForwardedHeadersOptions()
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
